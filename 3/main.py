9062eef49cff7576e2cb8498fae0483801b899f3
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-06 10:01:19 +0100

    day 3 part 2

--- 3/main.py
+++ 3/main.py
@@ -4,12 +4,21 @@ sum_of_muls = 0
 
 with open('input.txt', 'r') as file:
     s = file.read()
-    res = re.findall("mul\(\d+,\d+\)", s)
+    dos_donts_muls = re.findall(re.compile("(mul\(\d+,\d+\))|(do\(\))|(don't\(\))"), s)
+    print(dos_donts_muls)
+    do = True
 
-    for mul in res:
-        mul_split = mul.split(',')
-        n1 = int(mul_split[0][4:])
-        n2 = int(mul_split[1][:-1])
-        sum_of_muls += n1 * n2
+    for instruction in dos_donts_muls:
+        if instruction[0] != '':  # mul(x,y)
+            if not do:
+                continue
+            mul_split = instruction[0].split(',')
+            n1 = int(mul_split[0][4:])
+            n2 = int(mul_split[1][:-1])
+            sum_of_muls += n1 * n2
+        elif instruction[2] != '':  # don't()
+            do = False
+        else:  # do()
+            do = True
 
 print(sum_of_muls)
