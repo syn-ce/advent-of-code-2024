88c52a90e677d97c4b85ebf086146496934c8b89
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-06 14:50:30 +0100

    simplify day 2 part 2 (assume at least 4 nums per record)

--- 2/main.py
+++ 2/main.py
@@ -1,26 +1,27 @@
-safe_records = 0
-
-
 def valid_step(n1: int, n2: int, increasing: bool) -> bool:
     if not increasing:
         n1, n2 = n2, n1
-    if n1 >= n2 or n2 - n1 > 3:
-        return False
-    return True
+    return n1 < n2 and n2 - n1 <= 3
 
+
+safe_records = 0
 
 with open('input.txt', 'r') as file:
     for line in file:
         record = line.split()
         record = [int(n) for n in record]
-        increasing = record[0] < record[1]  # Assumes at least two numbers in record
+
+        # Determine whether increasing or decreasing (for 2 of 24 configs,
+        # we can't restore a sequence by eliminating just one number, but in
+        # that case we don't care since we'll fail anyway) ((3,1,4,2) and (3,4,1,2))
+        increasing = (int(record[0] < record[1]) + int(record[1] < record[2]) + int(
+            record[2] < record[3])) > 1  # Assumes at least four numbers in record
 
         safe = True
         removed_level = False
 
         # When encountering a problematic step (between n1 and n2),
         # test if removing n1 or n2 works locally, i.e. restores the order
-        # Also need to reevaluate whether we are increasing or decreasing
         i = 0
         while i < len(record) - 1:
             n1, n2 = record[i], record[i + 1]
@@ -28,21 +29,10 @@ with open('input.txt', 'r') as file:
                 if removed_level:
                     safe = False
                     break
-
                 removed_level = True
-                if i >= len(record) - 2 or valid_step(n1, record[i + 2], increasing):
-                    # TODO: check if below check (or something similar) is necessary
-                    # or (i == 0 and valid_step(n1, record[i + 2], not increasing)):  # Remove n2
-                    if i == 0:  # n2 is record[1] -> Reevaluate increasing
-                        increasing = record[0] < record[2]
+                if i >= len(record) - 2 or valid_step(n1, record[i + 2], increasing):  # Remove n2
                     i += 1  # Skip n2
-                elif i == 0:  # Remove n1
-                    increasing = record[1] < record[2]  # n1 is record[0] -> Reevaluate increasing
-                elif valid_step(record[i - 1], n2, increasing) or (
-                        i == 1 and valid_step(record[i - 1], n2, not increasing)):  # Remove n1
-                    if i == 1:  # n1 is record[0] -> Reevaluate increasing
-                        increasing = record[0] < record[2]
-                else:
+                elif i != 0 and not valid_step(record[i - 1], n2, increasing):  # Also can't remove n1
                     safe = False
                     break
             i += 1
