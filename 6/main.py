8f6ca2699c699ce739d23ba3f5a49d99925ad3c0
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-07 12:59:37 +0100

    day 6 part 2

--- 6/main.py
+++ 6/main.py
@@ -1,8 +1,16 @@
-def simulate(board: list[list[int]], row: int, col: int, xdir: int, ydir: int, visited: set[(int, int)]):
-    visited.add((row, col))
+def board_bounds(board: list[list[chr]], row: int, col: int) -> bool:
+    return not (row < 0 or row >= len(board) or col < 0 or col >= len(board[0]))
+
+
+def circle_in_simulation(board: list[list[chr]], row: int, col: int, xdir: int, ydir: int,
+                         visited: dict[(int, int), set[(int, int)]]):
+    visited[(row, col)] = {(xdir, ydir)}
+
     # While guard is on board
-    while 0 <= row + ydir < len(board) and 0 <= col + xdir < len(board[0]):
-        while board[row + ydir][col + xdir] == '#':
+    while board_bounds(board, row, col):
+        for i in range(4):
+            if not board_bounds(board, row + ydir, col + xdir) or board[row + ydir][col + xdir] != '#':
+                break
             # Need to turn
             # 0, -1 -> 1, 0 -> 0, 1 -> -1, 0 -> 0, -1
             if xdir == 0 and ydir == -1:
@@ -13,31 +21,56 @@ def simulate(board: list[list[int]], row: int, col: int, xdir: int, ydir: int, v
                 xdir, ydir = -1, 0
             else:
                 xdir, ydir = 0, -1
+        if not board_bounds(board, row + ydir, col + xdir):  # Left board
+            return False
+        if board[row + ydir][col + xdir] == '#':  # Still facing wall -> enclosed
+            return True
 
         # March forward
         row += ydir
         col += xdir
 
-        visited.add((row, col))
+        if (row, col) not in visited:
+            visited[(row, col)] = set()
+        if (xdir, ydir) in visited[(row, col)]:
+            return True
+        visited[(row, col)].add((xdir, ydir))
+
+    return False
 
 
-with open("input.txt", 'r') as file:
-    start_row = -1  # Careful, -1 is a valid index in python
-    start_col = -1
-    board: list[list[chr]] = []
-    # Construct board and find starting position
-    for i, line in enumerate(file):
-        board.append(list(line.strip()))
-        print(board[i])
-        if start_row == -1:
-            try:
-                idx = board[i].index('^')
-                start_row = i
-                start_col = idx
-            except ValueError:
-                pass
+def load_board(file_name: str) -> (list[list[chr]], int, int):
+    with open(file_name, 'r') as file:
+        start_row = -1  # Careful, -1 is a valid index in python
+        start_col = -1
+        board: list[list[chr]] = []
+        # Construct board and find starting position
+        for i, line in enumerate(file):
+            board.append(list(line.strip()))
+            if start_row == -1:
+                try:
+                    idx = board[i].index('^')
+                    start_row = i
+                    start_col = idx
+                except ValueError:
+                    pass
 
-    # Simulate starting from (start_row, start_col)
-    visited: set[(int, int)] = set()
-    simulate(board, start_row, start_col, 0, -1, visited)
-    print(len(visited))
+        return board, start_row, start_col
+
+
+board, start_row, start_col = load_board('input.txt')
+# Simulate starting from (start_row, start_col)
+visited: dict[(int, int), set[(int, int)]] = dict()
+
+circle_count = 0
+for row in range(len(board)):
+    print(f'{round(row / len(board), 3) * 100}%')
+    for col in range(len(board[0])):
+        if board[row][col] == '.':
+            board[row][col] = '#'
+            visited.clear()
+            if circle_in_simulation(board, start_row, start_col, 0, -1, visited):
+                circle_count += 1
+            board[row][col] = '.'
+
+print(circle_count)
