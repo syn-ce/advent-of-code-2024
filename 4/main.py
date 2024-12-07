a3d82bde6459be052685631f22d9387dd8fd4160
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-07 18:20:04 +0100

    improve day 4 part 2 (no need to keep track of direction of 'MAS')

--- 4/main.py
+++ 4/main.py
@@ -2,26 +2,28 @@
 # in one of two sets, depending on the direction of the 'MAS' (top left to bottom right, or bottom left to top right).
 # Then we can simply take the length of their intersection
 def dfs(board: list[list[chr]], row: int, col: int, word: str, idx: int, xdir: int, ydir: int,
-        tlbr_diag: set[(int, int)], bltr_diag: set[(int, int)]):
+        xmas_positions: set[(int, int)]) -> int:
     if row < 0 or row >= len(board) or col < 0 or col >= len(board[row]) or board[row][col] != word[idx]:
-        return
+        return 0
 
     if idx == len(word) - 1:
-        if xdir == 1 and ydir == 1 or xdir == -1 and ydir == -1:
-            tlbr_diag.add((row - xdir, col - ydir))  # Only works because 'MAS' is 3-letter word -> we have to go 1 back
-        else:
-            bltr_diag.add((row - xdir, col - ydir))
-        return
+        # Only works because 'MAS' is 3-letter word -> we have to go 1 back
+        middle_pos = (row - xdir, col - ydir)
+        if middle_pos in xmas_positions:  # Found second 'MAS' for this position -> must form cross
+            return 1
+        xmas_positions.add(middle_pos)
+        return 0
 
     if not (xdir == 0 and ydir == 0):  # Only check one direction
-        dfs(board, row + xdir, col + ydir, word, idx + 1, xdir, ydir, tlbr_diag, bltr_diag)
-        return
+        return dfs(board, row + xdir, col + ydir, word, idx + 1, xdir, ydir, xmas_positions)
 
+    cross_count = 0
     for i in [-1, 1]:  # Check diagonals
         for j in [-1, 1]:
             if i == 0 and j == 0:
                 continue
-            dfs(board, row + i, col + j, word, idx + 1, i, j, tlbr_diag, bltr_diag)
+            cross_count += dfs(board, row + i, col + j, word, idx + 1, i, j, xmas_positions)
+    return cross_count
 
 
 with open('input.txt', 'r') as file:
@@ -29,12 +31,11 @@ with open('input.txt', 'r') as file:
     for line in file:
         board.append(list(line.strip()))
 
-    appearances = 0
-    tlbr_diag: set[(int, int)] = set()
-    bltr_diag: set[(int, int)] = set()
+    cross_count = 0
+    xmas_positions = set()
     # Dfs on board
     for row in range(len(board)):
         for col in range(len(board[row])):
-            dfs(board, row, col, "MAS", 0, 0, 0, tlbr_diag, bltr_diag)
+            cross_count += dfs(board, row, col, 'MAS', 0, 0, 0, xmas_positions)
 
-    print(len(tlbr_diag.intersection(bltr_diag)))
+    print(cross_count)
