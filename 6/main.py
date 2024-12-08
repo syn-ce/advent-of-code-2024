52f0b6c2b0650e03898dd26dbc482c2c35d5b645
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-08 20:24:34 +0100

    refactor day 6 to have functions part1, part2; print progress in one line

--- 6/main.py
+++ 6/main.py
@@ -1,9 +1,28 @@
-def board_bounds(board: list[list[chr]], row: int, col: int) -> bool:
+def load_board(file_name: str) -> tuple[list[list[str]], int, int]:
+    with open(file_name, 'r') as file:
+        start_row = -1  # Careful, -1 is a valid index in python
+        start_col = -1
+        board: list[list[str]] = []
+        # Construct board and find starting position
+        for i, line in enumerate(file):
+            board.append(list(line.strip()))
+            if start_row == -1:
+                try:
+                    idx = board[i].index('^')
+                    start_row = i
+                    start_col = idx
+                except ValueError:
+                    pass
+
+        return board, start_row, start_col
+
+
+def board_bounds(board: list[list[str]], row: int, col: int) -> bool:
     return not (row < 0 or row >= len(board) or col < 0 or col >= len(board[0]))
 
 
-def circle_in_simulation(board: list[list[chr]], row: int, col: int, xdir: int, ydir: int,
-                         visited: dict[(int, int), set[(int, int)]]):
+def simulate_detect_cycle(board: list[list[str]], row: int, col: int, xdir: int, ydir: int,
+                          visited: dict[tuple[int, int], set[tuple[int, int]]]):
     visited[(row, col)] = {(xdir, ydir)}
 
     # While guard is on board
@@ -39,42 +58,40 @@ def circle_in_simulation(board: list[list[chr]], row: int, col: int, xdir: int, 
     return False
 
 
-def load_board(file_name: str) -> (list[list[chr]], int, int):
-    with open(file_name, 'r') as file:
-        start_row = -1  # Careful, -1 is a valid index in python
-        start_col = -1
-        board: list[list[chr]] = []
-        # Construct board and find starting position
-        for i, line in enumerate(file):
-            board.append(list(line.strip()))
-            if start_row == -1:
-                try:
-                    idx = board[i].index('^')
-                    start_row = i
-                    start_col = idx
-                except ValueError:
-                    pass
+def part1():
+    board, start_row, start_col = load_board('input.txt')
 
-        return board, start_row, start_col
+    # Simulate starting from (start_row, start_col)
+    visited: dict[(int, int), set[(int, int)]] = dict()
+    simulate_detect_cycle(board, start_row, start_col, 0, -1, visited)
+
+    return len(visited)
 
 
-board, start_row, start_col = load_board('input.txt')
+def part2():
+    board, start_row, start_col = load_board('input.txt')
 
-# Simulate starting from (start_row, start_col)
-visited: dict[(int, int), set[(int, int)]] = dict()
-circle_in_simulation(board, start_row, start_col, 0, -1, visited)
+    # Simulate starting from (start_row, start_col) -> get visited cells
+    visited: dict[tuple[int, int], set[tuple[int, int]]] = dict()
+    simulate_detect_cycle(board, start_row, start_col, 0, -1, visited)
 
-circle_count = 0
-del visited[(start_row, start_col)]  # Can't place obstacle at initial guard position
-# Only obstacles in _visited_ places make a difference
-for i, pos in enumerate(visited):
-    row, col = pos
-    if i % (len(visited) // 100) == 0:
-        print(f'{round(i * 100 / len(visited), 2)}%')
-    if board[row][col] == '.':
-        board[row][col] = '#'
-        if circle_in_simulation(board, start_row, start_col, 0, -1, dict()):
-            circle_count += 1
-        board[row][col] = '.'
+    circles = 0
+    del visited[(start_row, start_col)]  # Can't place obstacle at initial guard position
+    # Only obstacles placed at positions in `visited` will be reachable
+    for i, pos in enumerate(visited):
+        row, col = pos
+        if i % (len(visited) // 100) == 0:  # Printing progress
+            # Works in terminal, doesn't display in PyCharm (flushing doesn't fix that)
+            print(f'{round(i * 100 / len(visited), 2):.2f}%', end='\r')
+        if board[row][col] == '.':
+            board[row][col] = '#'
+            if simulate_detect_cycle(board, start_row, start_col, 0, -1, dict()):
+                circles += 1
+            board[row][col] = '.'
 
-print(circle_count)
+    print('\033[K', end='\r')  # Erase (potentially printed) progress
+    return circles
+
+
+print(part1())
+print(part2())
