9b248ef1e55daf70aea47f7b358d6c256333ff65
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-08 12:33:28 +0100

    refactor day 1 to have functions part1, part2

--- 1/main.py
+++ 1/main.py
@@ -1,22 +1,35 @@
-l1 = []
-l2 = []
-with open('input.txt', 'r') as file:
-    for line in file:
-        n1, n2 = line.split()
-        l1.append(int(n1))
-        l2.append(int(n2))
+def read_sort_lists(file_name: str) -> tuple[list[int], list[int]]:
+    l1 = []
+    l2 = []
+    with open(file_name, 'r') as file:
+        for line in file:
+            n1, n2 = line.split()
+            l1.append(int(n1))
+            l2.append(int(n2))
 
-l1.sort()
-l2.sort()
+    l1.sort()
+    l2.sort()
+    return l1, l2
 
-diff = sum([abs(n1 - n2) for n1, n2 in zip(l1, l2)])
 
-print(diff)
+def part1():
+    l1, l2 = read_sort_lists('input.txt')
 
-l1_frequencies = {}
+    diff = sum([abs(n1 - n2) for n1, n2 in zip(l1, l2)])
+    return diff
 
-for num in l1:
-    l1_frequencies[num] = l1_frequencies.get(num, 0) + 1
 
-similarity_score = sum([num * l1_frequencies.get(num, 0) for num in l2])
-print(similarity_score)
+def part2():
+    l1, l2 = read_sort_lists('input.txt')
+
+    l1_frequencies = {}
+
+    for num in l1:
+        l1_frequencies[num] = l1_frequencies.get(num, 0) + 1
+
+    similarity_score = sum([num * l1_frequencies.get(num, 0) for num in l2])
+    return similarity_score
+
+
+print(part1())
+print(part2())
