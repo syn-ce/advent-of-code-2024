ac5bc86a120abce6fb7de977ef78e1d3d72c0e06
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-25 22:55:55 +0100

    day 18 part 1: remove intermediate printing

--- 18/main.py
+++ 18/main.py
@@ -32,15 +32,12 @@ def bfs(rows: int, cols: int, illegal: set[tuple[int, int]]) -> int:
         row, col = q.popleft()
         cost = costs.popleft()
 
-        print(row, col)
-
         # Reached exit
         if row == rows - 1 and col == cols - 1:
             return cost
 
         # Can't / Shouldn't move here
         if row < 0 or row >= rows or col < 0 or col >= cols or (row, col) in illegal or (row, col) in visited:
-            print(f'Cannot do {row, col}')
             continue
 
         visited.add((row, col))
@@ -54,7 +51,6 @@ def bfs(rows: int, cols: int, illegal: set[tuple[int, int]]) -> int:
 
 def part1(memory_dimensions: tuple[int, int], nr_bytes: int) -> int:
     obstacles = load_obstacles('input.txt', nr_bytes)
-    print_memory(memory_dimensions[0], memory_dimensions[1], obstacles)
     return bfs(memory_dimensions[0], memory_dimensions[1], obstacles)
 
 
