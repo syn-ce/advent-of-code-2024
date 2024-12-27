eeccd83818a1e73c95c96b24e5c1620eac043d3d
Maximilian Amthor <amthormaximilian@gmail.com>
2024-12-27 01:11:14 +0100

    day 22 part 2

--- 22/main.py
+++ 22/main.py
@@ -30,4 +30,26 @@ def part1():
     return secret_sums
 
 
+def part2():
+    secrets = load_secrets('input.txt')
+    sequence_to_profit: dict[tuple[int, int, int, int], int] = dict()
+
+    for secret in secrets:
+        seen = set()
+        sequence = (0, 0, 0, 0)
+        for i in range(2000):
+            next_secret = step(secret)
+            diff = next_secret % 10 - secret % 10
+            sequence = (sequence[1], sequence[2], sequence[3], diff)
+            secret = next_secret
+            if i >= 3 and sequence not in seen:
+                sequence_to_profit[sequence] = sequence_to_profit.get(sequence, 0) + (secret % 10)
+                seen.add(sequence)
+
+    k = max(sequence_to_profit, key=sequence_to_profit.get)
+    print(k, sequence_to_profit[k])
+    return max(sequence_to_profit.values())
+
+
 print(part1())
+print(part2())
